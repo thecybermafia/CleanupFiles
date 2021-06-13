c1db5beb59b10a6992c599b8636de691db21ac5f
Rajan Christian <15846532+thecybermafia@users.noreply.github.com>
2021-06-13 13:46:04 +0530

    Create Script.ps1

---
+++ Script.ps1
@@ -0,0 +1,26 @@
+Param(
+    [string]$ConfigPath = ".\config.cfg",
+    [string]$FolderPath = "C:\Users\$env:UserName\Downloads\",
+    [string]$LogPath = "."
+)
+
+$Config = Import-Csv -Path $ConfigPath
+$Files = $FolderPath | Get-ChildItem
+$LogName = "sdlog_$(Get-Date -Format MMddyyyy-hhmmss).txt"   
+New-Item -Path $LogPath -Name $LogName
+
+ForEach($Extension in $Config) {
+    ForEach($File in $Files) {
+        if($File.Extension -eq $Extension.Extension) {
+            if(!(Test-Path $Extension.Path -PathType Container)) {
+                New-Item -Path $Extension.Path -ItemType Directory
+            }
+            Move-Item (Join-Path $FolderPath $File) $Extension.Path -Force
+            Add-Content -Path (Join-Path $LogPath $LogName) -Value "Moved <$File> from <$FolderPath> to <$($Extension.Path)>"
+        }    
+    }
+}
+
+if(!(Get-Content $LogName)){
+    Remove-Item $LogName
+}
